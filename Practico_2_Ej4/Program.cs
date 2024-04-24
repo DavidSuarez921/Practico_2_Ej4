73e727f094aef2fba5ac09bbc39867c7a19fcd19
David Jonatan <davidjonatan@gmail.com>
2024-04-24 12:59:56 -0300

    Se agrega el Proyecto

---
+++ Practico_2_Ej4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practico_2_Ej4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> valores = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, };
+            
+            var sumaTotal = valores.Sum();
+            Console.WriteLine("La suma total es: " + sumaTotal.ToString());
+
+            var sumaTotalPares = valores.Where(x => x % 2 == 0).Sum() ;
+            
+            
+                Console.WriteLine("La suma total de los valores pares es: " + sumaTotalPares);
+            
+        }
+    }
+}
